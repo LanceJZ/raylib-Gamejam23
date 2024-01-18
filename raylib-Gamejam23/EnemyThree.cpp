b59430bd416ca4f3b06da5125897ccf606cbe8eb
LanceJZ <lance.zimmerman@gmail.com>
2024-01-18 15:15:04 -0800

    Enemy Three attack player now functions.

--- raylib-Gamejam23/EnemyThree.cpp
+++ raylib-Gamejam23/EnemyThree.cpp
@@ -18,6 +18,7 @@ bool EnemyThree::Initialize(Utilities* utilities)
 	Enemy::Initialize(utilities);
 
 	ThrustTimer = TheManagers.EM.AddTimer();
+	ShotTimerTime = 1.5f;
 
 	return true;
 }
@@ -84,7 +85,7 @@ void EnemyThree::Patrolling(float deltaTime)
 	Vector3 heading = CheckOtherSide(Waypoints[WaypointNumber]);
 
 	SetHeading(heading, 0.3666f);
-	SetAccelerationToMaxAtRotation(Thrust, 0.055f, deltaTime);
+	SetAccelerationToMaxAtRotation(Thrust, 0.05f, deltaTime);
 
 	float distance = Vector3Distance(heading, Position);
 
@@ -93,10 +94,34 @@ void EnemyThree::Patrolling(float deltaTime)
 		if (WaypointNumber < 3) WaypointNumber++;
 		else WaypointNumber = 0;
 	}
+
+	float playerDistance = Vector3Distance(heading, ThePlayer->Position);
+
+	if (playerDistance < 300) InState = AIState::AttackPlayer;
 }
 
 void EnemyThree::Attack(float deltaTime)
 {
+	Vector3 heading = CheckOtherSide(Waypoints[WaypointNumber]);
+	Vector3 playerAt = CheckOtherSide(ThePlayer->Position);
+	float playerDistance = Vector3Distance(heading, ThePlayer->Position);
+
+	if (TheManagers.EM.Timers[ThrustTimer]->Elapsed())
+	{
+		Thrust = GetRandomFloat(10.0f, 40.0f);
+		TheManagers.EM.Timers[ThrustTimer]->Reset(3.0f);
+	}
+
+	if (TheManagers.EM.Timers[ShotTimer]->Elapsed())
+	{
+		Fire();
+		TheManagers.EM.Timers[ShotTimer]->Reset(3.15f);
+	}
+
+	SetHeading(playerAt, 0.4666f);
+	SetAccelerationToMaxAtRotation(Thrust, 0.025f, deltaTime);
+
+	if (playerDistance > 500) InState = AIState::Patrol;
 }
 
 void EnemyThree::Spawn(Vector3 position)
